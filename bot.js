1f9a7b3896d21409b6b7687f98c4075c972a02c0
Sandun Isuru Niraj <sandun.isuru@gmail.com>
2018-10-06 03:49:43 +0530

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityTypes,
+  CardFactory,
+  ConversationState,
+  TurnContext } = require('botbuilder');
+
+const { ChoicePrompt,
+  DialogSet,
+  DialogTurnResult,
+  DialogTurnStatus,
+  ListStyle } = require('botbuilder-dialogs');
+
+// Turn counter property
+const TURN_COUNTER_PROPERTY = 'turnCounterProperty';
+
+class MyBot {
+  /**
+   *
+   * @param {ConversationState} conversation state object
+   */
+  constructor(conversationState) {
+   
+    this.conversationState = conversationState;
+    this.dialogState = conversationState.createProperty('dialogState');
+    this.dialogs = new DialogSet(this.dialogState);
+    const prompt = new ChoicePrompt('cardPrompt');
+    prompt.style = ListStyle.list;
+    this.dialogs.add(prompt);
+  }
+  /**
+   *
+   * @param {TurnContext} on turn context object.
+   */
+  async onTurn(turnContext) {
+
+
+    if (turnContext.activity.type === ActivityTypes.Message) {
+      const dc = await this.dialogs.createContext(turnContext);
+
+      const results = await dc.continueDialog();
+      
+      if (!turnContext.responded && results.status === DialogTurnStatus.empty) {
+
+          
+
+          // Prompt the user with the configured PromptOptions.
+          await dc.prompt('cardPrompt', promptOptions);
+          }
+      // The bot parsed a valid response from user's prompt response and so it must respond.
+          } else if (results.status === DialogTurnStatus.complete) {
+            await this.sendCardResponse(turnContext, results);
+          }
+            await this.conversationState.saveChanges(turnContext);
+    }
+    // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+    // if (turnContext.activity.type === ActivityTypes.Message) {
+    //   // read from state.
+    //   let count = await this.countProperty.get(turnContext);
+    //   count = count === undefined ? 1 : ++count;
+    //   await turnContext.sendActivity(`${count}: You said "${turnContext.activity.text}"`);
+    //   // increment and set turn counter.
+    //   await this.countProperty.set(turnContext, count);
+    // } else {
+    //   await turnContext.sendActivity(`[${turnContext.activity.type} event detected]`);
+    // }
+    // Save state changes
+          await this.conversationState.saveChanges(turnContext);
+    }
+
+
+
+}
+
+module.exports.MyBot = MyBot;
